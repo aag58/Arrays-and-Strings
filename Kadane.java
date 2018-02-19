44e2f3f063de3ce0094e3d31bbeafa47b69474a2
aag58 <aag58@njit.edu>
2018-02-18 21:29:45 -0500

    Added file

---
+++ Kadane.java
@@ -0,0 +1,22 @@
+public class Kadane {
+
+    public static int subarray(int[] arr){
+        int max = 0;
+        int sum = 0;
+
+        for (int i = 0; i < arr.length ; i++) {
+            sum = sum + arr[i];
+            if(sum < 0){
+                sum = 0;
+            }
+            if(max < sum){
+                max = sum;
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
+        System.out.println(subarray(arr));
+    }
+}
